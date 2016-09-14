8423640e911aad2aec6085ce794f80de0c9d1c66
Sergey Kuravskiy <s_kuravskiy@wargaming.net>
2016-09-14 20:28:38 +0300

    Core factory validation type

--- Game.Core/CoreFactory.cs
+++ Game.Core/CoreFactory.cs
@@ -9,15 +9,21 @@ namespace Game.Core
         public static ICoreFactory<T> GetFactory()
         {
             if (typeof(T) == typeof(IPlayer))
-                return new CoreFactoryGeneric<Player>() as ICoreFactory<T>;
+            {
+                return new CoreFactoryGeneric<Player>() as ICoreFactory<IPlayer>;
             else
                 return null;
-        }
+            }
 
-        private class CoreFactoryGeneric<Tobj> : ICoreFactory<T> where Tobj : new()
+        private class CoreFactoryGeneric<Tobj> : ICoreFactory<T> where Tobj : T, new()
         {
             public T Create()
             {
+                var type = typeof(Tobj);
+
+                if (type.IsInterface || type.IsAbstract)
+                    throw new ArgumentException("Can't implement interface or abstract class");
+
                 return new Tobj();
             }
         }
